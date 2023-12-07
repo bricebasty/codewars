4e211ee68204ab64731d8960989825b7537b11a5
bricebasty <brice.basty1@gmail.com>
2023-12-07 08:14:38 +0000

    Dec 7, 2023, 9:14 AM

--- py/who_likes_it.py
+++ py/who_likes_it.py
@@ -5,12 +5,40 @@
 # Implement the function which takes an array containing the names of people
 # that like an item. It must return the display text as shown in the examples:
 
-# []                                -->  "no one likes this"
-# ["Peter"]                         -->  "Peter likes this"
-# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
-# Note: For 4 or more names, the number in "and 2 others" simply increases.
+# PROBLEM
+# ----------------
+#
+# INPUT: Array
+# OUTPUT:
+#
+# RULES:
+# -
+#
+# EXAMPLES
+# ----------------
+#
+# INPUT:
+# =>
+# =>
+# OUTPUT:
+#
+# DATA STRUCTURES
+# ----------------
+#
+# INPUT:
+# =>
+# OUTPUT:
+#
+# ALGORITHM
+# ----------------
+#
+#
+# NOTES:
+#
+# WHAT:
+#
+# HOW:
+#
 
 def likes(names):
     # your code here
