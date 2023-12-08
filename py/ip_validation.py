7fbb845a295327ae3467b2b743085526f5ad3d5b
bricebasty <brice.basty1@gmail.com>
2023-12-08 08:44:41 +0000

    Dec 8, 2023, 9:44 AM

--- py/ip_validation.py
+++ py/ip_validation.py
@@ -79,17 +79,17 @@ def is_valid_IP(string):
           return False
     return True
 
-# print('Test result is ' + str(is_valid_IP('12.255.56.1') ==     True))
-# print(is_valid_IP('12.255.56.1')) #     True
+print('Test result is ' + str(is_valid_IP('12.255.56.1') ==     True))
+print(is_valid_IP('12.255.56.1')) #     True
 
-# print('Test result is ' + str(is_valid_IP('') ==                False))
-# print(is_valid_IP('')) #                False
+print('Test result is ' + str(is_valid_IP('') ==                False))
+print(is_valid_IP('')) #                False
 
-# print('Test result is ' + str(is_valid_IP('abc.def.ghi.jkl') == False))
-# print(is_valid_IP('abc.def.ghi.jkl')) # False
+print('Test result is ' + str(is_valid_IP('abc.def.ghi.jkl') == False))
+print(is_valid_IP('abc.def.ghi.jkl')) # False
 
-# print('Test result is ' + str(is_valid_IP('123.456.789.0') ==   False))
-# print(is_valid_IP('123.456.789.0')) #   False
+print('Test result is ' + str(is_valid_IP('123.456.789.0') ==   False))
+print(is_valid_IP('123.456.789.0')) #   False
 
 print('Test result is ' + str(is_valid_IP('12.34.56') ==        False))
 print(is_valid_IP('12.34.56')) #        False
@@ -100,17 +100,17 @@ print(is_valid_IP('12.34.56 .1')) #     False
 print('Test result is ' + str(is_valid_IP('12.34.56.-1') ==     False))
 print(is_valid_IP('12.34.56.-1')) #     False
 
-# print('Test result is ' + str(is_valid_IP('123.045.067.089') == False))
-# print(is_valid_IP('123.045.067.089')) # False
+print('Test result is ' + str(is_valid_IP('123.045.067.089') == False))
+print(is_valid_IP('123.045.067.089')) # False
 
-# print('Test result is ' + str(is_valid_IP('127.1.1.0') ==        True))
-# print(is_valid_IP('127.1.1.0')) #        True
+print('Test result is ' + str(is_valid_IP('127.1.1.0') ==        True))
+print(is_valid_IP('127.1.1.0')) #        True
 
-# print('Test result is ' + str(is_valid_IP('0.0.0.0') ==          True))
-# print(is_valid_IP('0.0.0.0')) #          True
+print('Test result is ' + str(is_valid_IP('0.0.0.0') ==          True))
+print(is_valid_IP('0.0.0.0')) #          True
 
-# print('Test result is ' + str(is_valid_IP('0.34.82.53') ==       True))
-# print(is_valid_IP('0.34.82.53')) #       True
+print('Test result is ' + str(is_valid_IP('0.34.82.53') ==       True))
+print(is_valid_IP('0.34.82.53')) #       True
 
-# print('Test result is ' + str(is_valid_IP('192.168.1.300') ==   False))
-# print(is_valid_IP('192.168.1.300')) #   False
+print('Test result is ' + str(is_valid_IP('192.168.1.300') ==   False))
+print(is_valid_IP('192.168.1.300')) #   False
