29efe92672ac26337796f0bf89df61e9c1869869
bricebasty <brice.basty1@gmail.com>
2023-12-08 08:04:49 +0000

    Dec 8, 2023, 9:04 AM

--- py/ip_validation.py
+++ py/ip_validation.py
@@ -18,11 +18,13 @@
 # PROBLEM
 # ----------------
 #
-# INPUT:
-# OUTPUT:
+# INPUT: String
+# OUTPUT: Boolean (format of IP adresss is valid?)
 #
 # RULES:
-# -
+# - Leading zeros are considered invalid
+# - Inputs will be only single string
+# - 4 numbers from 1 to 255 separated by 3 dots
 #
 # EXAMPLES
 # ----------------
@@ -49,3 +51,42 @@
 #
 # HOW:
 #
+
+def is_valid_IP(strng):
+    return None
+
+print('Test result is ' + str(is_valid_IP('12.255.56.1') ==     True))
+print(is_valid_IP('12.255.56.1')) #     True
+
+print('Test result is ' + str(is_valid_IP('') ==                False))
+print(is_valid_IP('')) #                False
+
+print('Test result is ' + str(is_valid_IP('abc.def.ghi.jkl') == False))
+print(is_valid_IP('abc.def.ghi.jkl')) # False
+
+print('Test result is ' + str(is_valid_IP('123.456.789.0') ==   False))
+print(is_valid_IP('123.456.789.0')) #   False
+
+print('Test result is ' + str(is_valid_IP('12.34.56') ==        False))
+print(is_valid_IP('12.34.56')) #        False
+
+print('Test result is ' + str(is_valid_IP('12.34.56 .1') ==     False))
+print(is_valid_IP('12.34.56 .1')) #     False
+
+print('Test result is ' + str(is_valid_IP('12.34.56.-1') ==     False))
+print(is_valid_IP('12.34.56.-1')) #     False
+
+print('Test result is ' + str(is_valid_IP('123.045.067.089') == False))
+print(is_valid_IP('123.045.067.089')) # False
+
+print('Test result is ' + str(is_valid_IP('127.1.1.0') ==        True))
+print(is_valid_IP('127.1.1.0')) #        True
+
+print('Test result is ' + str(is_valid_IP('0.0.0.0') ==          True))
+print(is_valid_IP('0.0.0.0')) #          True
+
+print('Test result is ' + str(is_valid_IP('0.34.82.53') ==       True))
+print(is_valid_IP('0.34.82.53')) #       True
+
+print('Test result is ' + str(is_valid_IP('192.168.1.300') ==   False))
+print(is_valid_IP('192.168.1.300')) #   False
